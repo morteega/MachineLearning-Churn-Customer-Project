f7fb86a3b3a18b2afe55e5940f117fc80d0cd1ad
mortega <markisorte@icloud.com>
2025-12-28 20:25:50 +0100

    Clase DataLoader terminada

--- DataLoader.py
+++ DataLoader.py
@@ -3,18 +3,30 @@ import numpy as np
 
 class DataLoader:
     def __init__(self, path):
-        self.path = path
-        self.data = None
+        self.file_path=path
         
-    def load_data(file_path):
+    def load_data(self):
         try:
-            data=pd.read_csv(file_path)
-            return data
+            self.data=pd.read_csv(self.file_path)
+            self.data['TotalCharges']=pd.to_numeric(self.data['TotalCharges'],errors='coerce')
+            return self.data
         except FileNotFoundError:
-            print("File wasnt found.")
+            print("File wasn`t founf.")
             return None
         except pd.errors.EmptyDataError:
             print("File is empty.")
             return None
+            
         
-    def process_data(self)
+    def cehck_null_data(self):
+        return self.data.isnull().sum(),self.data.info()
+    
+    def fill_null_data(self):
+        self.data['TotalCharges']=self.data['TotalCharges'].fillna(0)
+        return self.data.isnull().sum()
+    # This function fills the null values in totalcharges column with the median of the column but it should be implemente in DataPreprocessor class
+    # not here.
+    
+    def check_balance(self):
+        return self.data['Churn'].value_counts(normalize=True)
+    # This function is just to know how unbalanced the data is
